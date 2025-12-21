e78048a2e5f96f5559b1d3be0a8d714981dd5429
KRITIKA233CODER <kritikaniranjandangi@gmail.com>
2025-12-21 15:14:40 +0530

    changes for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,40 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoutes from './routes/auth.route.js';
-import userRoutes from './routes/user.route.js';
-import chatRoutes from './routes/chat.route.js';
-
-import { connectDB } from './lib/db.js';
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+];
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://streamify.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/chat",chatRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
   connectDB();
 });
