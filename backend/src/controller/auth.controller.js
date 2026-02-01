f19ab6b1a5e998f64b2d832221c6918a66c39138
KRITIKA233CODER <kritikaniranjandangi@gmail.com>
2026-02-01 17:52:28 +0530

    pushing

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -91,8 +91,8 @@ export async function login(req, res) {
 
       res.cookie("jwt",token,{
         httpOnly:true,
-        secure:process.env.NODE_ENV==='production',
-        sameSite:'Strict',
+        secure:true,
+        sameSite:'None',
         maxAge:7*24*60*60*1000
       });
 
